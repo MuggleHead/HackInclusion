2b96a07328e6848df06f1793627fd21387b4b4cd
jass1204 <100995811+jass1204@users.noreply.github.com>
2022-05-15 09:56:38 +0530

    Add files via upload

---
+++ sellerscript.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("form"),
+fileInput = document.querySelector(".file-input"),
+progressArea = document.querySelector(".progress-area"),
+uploadedArea = document.querySelector(".uploaded-area");
+form.addEventListener("click", () =>{
+  fileInput.click();
+});
+fileInput.onchange = ({target})=>{
+  let file = target.files[0];
+  if(file){
+    let fileName = file.name;
+    if(fileName.length >= 12){
+      let splitName = fileName.split('.');
+      fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
+    }
+    uploadFile(fileName);
+  }
+}
